d0cdd72a3ffea72c187326c9802ac362445abec7
Constantin Zackl <constantin.zackl@gmail.com>
2024-04-11 16:18:13 +0200

    changed variable name

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var map = L.map('map').setView([-45.87, 170.5], 13);
+let lat = -45.87;
+let lng = 170.5;
+
+let map = L.map('map').setView([lat, lng], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([-45.87, 170.5]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+let marker = L.marker([lat, lng]).addTo(map);
+marker.bindPopup(`
+    <h2>Dunedin</h2>   
+    <ul>
+        <li>Breite: ${lat.toFixed(5)}</li>
+        <li>Länge: ${lng.toFixed(5)}</li>
+    </ul>
+`).openPopup(); 
