ad18933d70dd665be301d1ce85f0f28e7961df66
Constantin Zackl <constantin.zackl@gmail.com>
2024-04-11 17:36:59 +0200

    updated popup

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 let lat = -45.87;
 let lng = 170.5;
+let zoom = 13;
 
-let map = L.map('map').setView([lat, lng], 13);
+let map = L.map('map', { center: [lat, lng], zoom: zoom });
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-let marker = L.marker([lat, lng]).addTo(map);
-marker.bindPopup(`
-    <h2>Dunedin</h2>   
+
+L.control.scale({ metric: true, imperial: false }).addTo(map);
+
+let jsonPunkt = {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [lng, lat]
+    },
+    "properties": {
+        "name": "Dunedin"
+    }
+}
+
+L.geoJSON(jsonPunkt, {
+    style: function (feature) {
+        return { color: feature.properties.color };
+    }
+}).bindPopup(function (layer) {
+    return `
+    <h2>${layer.feature.properties.name}</h2>   
     <ul>
-        <li>Breite: ${lat.toFixed(5)}</li>
-        <li>Länge: ${lng.toFixed(5)}</li>
+        <li>Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)}</li>
+        <li>Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)}</li>
     </ul>
-`).openPopup(); 
+`;
+}).addTo(map);
